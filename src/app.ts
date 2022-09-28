682966b15252ee161605458a3727c50eb4614968
fe-ktch <sangarefantha@gmail.com>
2022-09-28 08:36:58 +0200

    up

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+/*
+Sallai András
+Szoft II N
+2022-09-21
+*/
+
+const calcButton = document.querySelector('#calcButton');
+const eredmeny = document.querySelector('#eredmeny');
+
+calcButton.addEventListener('click', () => {
+    const base = document.querySelector('#base') as HTMLInputElement;
+    const height = document.querySelector('#height') as HTMLInputElement;
+    let baseNum: number = Number(base.value);
+    let heightNum : number = Number(height.value);
+    let area = calcArea(baseNum, heightNum);
+    eredmeny.textContent = String(area); 
+    //alert(area);
+});
+
+function calcArea(base: number, height: number):number {
+    return base * height / 2;
+}
